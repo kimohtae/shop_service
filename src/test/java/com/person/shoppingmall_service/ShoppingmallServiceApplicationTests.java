f3ced56ba8cecc4c29638a5f7d335a7474aa5ac3
kimohtae <dhxo7707@naver.com>
2022-02-04 11:56:26 +0900

    첫 번째 프로젝트 파일 업로드

---
+++ src/test/java/com/person/shoppingmall_service/ShoppingmallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.person.shoppingmall_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
